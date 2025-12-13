67f37e6737f1ea6d0f302f4cbf3ed7231cee4636
Evgeny <bntk96lg@mail.ru>
2025-12-14 01:01:01 +0500

    Итоговое задание 10 спринта - 2 итерация.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -17,6 +17,25 @@ public class FilmController {
 
     private final Map<Long, Film> films = new HashMap<>();
 
+    private void validate(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.warn("Ошибка валидации: название фильма пустое");
+            throw new ValidationException("Название фильма не может быть пустым");
+        }
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            log.warn("Ошибка валидации: описание фильма слишком длинное");
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        }
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn("Ошибка валидации: дата релиза слишком ранняя");
+            throw new ValidationException("Дата релиза не может быть раньше 28.12.1895");
+        }
+        if (film.getDuration() <= 0) {
+            log.warn("Ошибка валидации: отрицательная продолжительность");
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
+    }
+
     @GetMapping
     public Collection<Film> findAll() {
         return films.values();
@@ -48,24 +67,5 @@ public class FilmController {
         films.put(film.getId(), film);
         log.info("Обновлён фильм: {}", film);
         return film;
-    }
-
-    private void validate(Film film) {
-        if (film.getName() == null || film.getName().isBlank()) {
-            log.warn("Ошибка валидации: название фильма пустое");
-            throw new ValidationException("Название фильма не может быть пустым");
-        }
-        if (film.getDescription() != null && film.getDescription().length() > 200) {
-            log.warn("Ошибка валидации: описание фильма слишком длинное");
-            throw new ValidationException("Максимальная длина описания — 200 символов");
-        }
-        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            log.warn("Ошибка валидации: дата релиза слишком ранняя");
-            throw new ValidationException("Дата релиза не может быть раньше 28.12.1895");
-        }
-        if (film.getDuration() <= 0) {
-            log.warn("Ошибка валидации: отрицательная продолжительность");
-            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
-        }
     }
 }
