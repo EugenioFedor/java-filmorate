9a12dcff6a2214af1edf199d78c696d7ae4a6d5a
Evgeny <bntk96lg@mail.ru>
2026-03-18 18:35:46 +0500

    Спринт 12, итоговое задание - исправлены замечания после ревью. 3 итерация.

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -23,11 +23,11 @@ public class UserService {
     public User create(User user) {
 
         if (user.getEmail() == null || !user.getEmail().contains("@")) {
-            throw new ValidationException("Invalid email");
+            throw new ValidationException("Email должен содержать символ @");
         }
 
-        if (user.getLogin() == null || user.getLogin().isBlank()) {
-            throw new ValidationException("Login cannot be empty");
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
         }
 
         if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
